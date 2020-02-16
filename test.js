540d3761b3a7d90aab7ffe6d90f94595b8f10072
Richie Bendall <richiebendall@gmail.com>
2020-02-16 19:29:49 +1300

    chore: Update deps
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- test.js
+++ test.js
@@ -2,12 +2,12 @@ const test = require("ava")
 const theModule = require(".")
 
 test("main", (t) => {
-	t.throws(() => {
-		theModule(123)
-	}, {
-		instanceOf: TypeError,
-		message: "Expected a string, got number",
-	})
+    t.throws(() => {
+        theModule(123)
+    }, {
+        instanceOf: TypeError,
+        message: "Expected a string, got number",
+    })
 
-	t.is(theModule("unicorns"), "unicorns & rainbows")
+    t.is(theModule("unicorns"), "unicorns & rainbows")
 })
