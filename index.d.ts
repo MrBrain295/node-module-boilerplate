9ef168457c0fc453ba7aaf0fc498c99550e30818
Richie Bendall <richiebendall@gmail.com>
2020-04-04 23:53:43 +1300

    Unindent typing comment
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
 /**
- * My awesome module.
- * @param input Lorem ipsum.
- * @param postfix Lorem ipsum.
- * @example
- * ```
- * const theModule = require("the-module");
- * theModule("unicorns");
- * //=> 'unicorns & rainbows'
- * ```
+My awesome module.
+@param input Lorem ipsum.
+@param postfix Lorem ipsum.
+@example
+```
+const theModule = require("the-module");
+theModule("unicorns");
+//=> 'unicorns & rainbows'
+```
 */
 declare function theModule(input: string, { postfix }: { postfix?: string }): string
 
